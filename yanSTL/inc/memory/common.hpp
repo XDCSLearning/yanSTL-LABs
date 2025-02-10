0fea8ee06e017f1ec4c105f35a94274144b51c5a
yqzhang4480 <zyq4480@qq.com>
2025-02-10 11:02:04 +0800

    feat(lab3): unique_ptr framework

--- yanSTL/inc/memory/common.hpp
+++ yanSTL/inc/memory/common.hpp
@@ -9,6 +9,12 @@ namespace my {
 template <typename T, typename Deleter = std::default_delete<T>>
 class unique_ptr;
 
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args&&... args);
+
+template <typename T>
+unique_ptr<T[]> make_unique(size_t size);
+
 template <typename T>
 class shared_ptr;
 
@@ -17,5 +23,8 @@ class weak_ptr;
 
 template <typename T>
 class enable_shared_from_this;
+
+template <typename T, typename... Args>
+shared_ptr<T> make_shared(Args&&... args);
 
 } // namespace my
