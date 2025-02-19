b2f89215a4a76600d9d74dd202879f840353710e
张英奇 Zhang Yingqi <zyq4480@qq.com>
2025-02-19 10:36:46 +0000

    feat(lab3):framework

--- yanSTL/inc/memory/common.hpp
+++ yanSTL/inc/memory/common.hpp
@@ -6,6 +6,7 @@
 
 namespace my {
 
+#ifndef DISMISS_UNIQUE_PTR
 template <typename T, typename Deleter = std::default_delete<T>>
 class unique_ptr;
 
@@ -14,17 +15,24 @@ unique_ptr<T> make_unique(Args&&... args);
 
 template <typename T>
 unique_ptr<T[]> make_unique(size_t size);
+#endif
 
+#ifndef DISMISS_SHARED_AND_WEAK_PTR
 template <typename T>
 class shared_ptr;
 
-template <typename T>
-class weak_ptr;
-
-template <typename T>
-class enable_shared_from_this;
-
 template <typename T, typename... Args>
 shared_ptr<T> make_shared(Args&&... args);
+
+template <typename T>
+class weak_ptr;
+#endif
+
+#ifndef DISMISS_ENABLE_SHARED_FROM_THIS
+template <typename T>
+class enable_shared_from_this;
+#endif
+
+
 
 } // namespace my
--- yanSTL/inc/yan_memory.hpp
+++ yanSTL/inc/yan_memory.hpp
@@ -1,4 +1,16 @@
 #pragma once
-#include "memory/unique_ptr.hpp"
-#include "memory/shared_ptr.hpp"
 
+// 当完成相关实现时，删除对应的宏定义。
+#define DISMISS_UNIQUE_PTR
+#define DISMISS_SHARED_AND_WEAK_PTR
+#define DISMISS_ENABLE_SHARED_FROM_THIS
+
+#include "memory/common.hpp"
+
+#ifndef DISMISS_UNIQUE_PTR
+#include "memory/unique_ptr.hpp"
+#endif
+
+#ifndef DISMISS_SHARED_AND_WEAK_PTR
+#include "memory/shared_ptr.hpp"
+#endif
