6007fb4b7ee68698ce87727a2027beeaf34b6ee5
Kaijie Gu <2459548460@qq.com>
2025-02-20 22:25:52 +0800

    lab 2 passed

--- yanSTL/inc/yan_allocator.hpp
+++ yanSTL/inc/yan_allocator.hpp
@@ -13,13 +13,13 @@ using size_t = std::size_t;
 class _alloc_proxy
 {
 public:
-    class memory_leak : public std::exception
+    class memory_leak : public std::runtime_error
     {
     public:
         memory_leak() :
-            std::exception("memory leak") {}
+            std::runtime_error("memory leak") {}
         memory_leak(size_t bytes, size_t allocations) :
-            std::exception(std::format("memory leaked with {} bytes in {} allocations", bytes, allocations).c_str()) {}
+            std::runtime_error(std::format("memory leaked with {} bytes in {} allocations", bytes, allocations).c_str()) {}
     };
 
     static _alloc_proxy& get_instance()
@@ -114,21 +114,25 @@ public:
         size_type __n = n;
 	    if (__builtin_mul_overflow(__n, sizeof(T), &__n))
 	      std::__throw_bad_array_new_length();
-        return static_cast<T*>(_proxy().allocate(n));
+        return static_cast<T*>(_proxy().allocate(n * sizeof(T)));
     }
     // 分配至少可容纳n个元素，实际上可容纳不小于n的最小的2的幂个元素的未初始化连续存储空间。
     [[nodiscard]] constexpr allocation_result<T*, size_type>
         allocate_at_least(size_type n)
     {
         /// 在此处添加你的实现。
-        size_type __n = std::pow(2, n);
+        std::size_t __n = 1;
+        while(__n < n){
+            __n = (__n << 1);
+        }
+        std::cout<<"__n:"<<__n <<"n:"<<n<<std::endl;
         return {allocate(__n), __n};
     }
     // 回收p所指示的、可容纳n个元素的存储空间。
     constexpr void deallocate(T* p, size_type n)
     {
         /// 在此处添加你的实现。
-        _proxy().deallocate(p, n);
+        _proxy().deallocate(p, n * sizeof(T));
     }
 
     // 判断同一类模板定义的各分配器实例类型的两个对象是否相等。
@@ -182,11 +186,33 @@ protected:
     template<typename _Tp>
       using __pocs = typename _Tp::propagate_on_container_swap;
     template<typename _Tp>
-      using __equal = std::__type_identity<typename _Tp::is_always_equal>;
-    template<typename _Tp>
-      using __diff = typename _Tp::difference_type;
-};
+      using __equal = std::__type_identity_t<typename _Tp::is_always_equal>;
 
+    template<typename _Tp, typename _Up, typename = void>
+      struct __rebind : std::__replace_first_arg<_Tp, _Up>
+      {
+	static_assert(std::is_same<
+	  typename std::__replace_first_arg<_Tp, typename _Tp::value_type>::type,
+			_Tp>::value,
+	  "allocator_traits<A>::rebind_alloc<A::value_type> must be A");
+      };
+
+    template<typename _Tp, typename _Up> requires requires{
+        typename _Tp::template rebind<_Up>::other;
+    }
+      struct __rebind<_Tp, _Up> 
+      {
+	using type = typename _Tp::template rebind<_Up>::other;
+
+	static_assert(std::is_same<
+	  typename _Tp::template rebind<typename _Tp::value_type>::other,
+			_Tp>::value,
+	  "allocator_traits<A>::rebind_alloc<A::value_type> must be A");
+      };
+};
+template<typename _Alloc, typename _Up>
+using __alloc_rebind
+    = typename __allocator_traits_base::template __rebind<_Alloc, _Up>::type;
 template <typename Alloc> 
 struct allocator_traits : __allocator_traits_base
 {
@@ -224,19 +250,19 @@ public:
     using const_pointer = _Ptr<__c_pointer, const value_type>;
     using void_pointer = _Ptr<__v_pointer, void>;
     using const_void_pointer =_Ptr<__cv_pointer, const void>;
-    using difference_type = _Diff<Alloc, pointer>;
+    using difference_type = _Diff<Alloc, pointer>::type;
     using size_type = typename _Size<Alloc, difference_type>::type;
     using propagate_on_container_copy_assignment = __detected_or_t<std::false_type, __pocca, Alloc>;
     using propagate_on_container_move_assignment = __detected_or_t<std::false_type, __pocma, Alloc>;
     using propagate_on_container_swap = __detected_or_t<std::false_type, __pocs, Alloc>;
-    using is_always_equal = true;
+    using is_always_equal = __detected_or_t<std::true_type, __equal, Alloc>; 
 
     // Member alias templates
     /// 你需要修改在此处的代码。
     template <typename T>
-    using rebind_alloc = void;
+    using rebind_alloc = __alloc_rebind<Alloc, T>;
     template <typename T>
-    using rebind_traits = void;
+    using rebind_traits = allocator_traits<rebind_alloc<T>>;
 
     // Member functions
     // 使用 a 申请 n 个 value_type 类型的元素所需的存储空间
@@ -250,7 +276,7 @@ public:
     [[nodiscard]] static constexpr pointer allocate(Alloc& a, size_type n, const_void_pointer hint)
     {
         /// 在此处添加你的实现。
-        if(requires{
+        if constexpr(requires{
             a.allocate(n, hint);
         }){
             return a.allocate(n, hint);
@@ -264,12 +290,19 @@ public:
         allocate_at_least(Alloc& a, size_type n)
     {
         /// 在此处添加你的实现。
-        return { nullptr, 0 };
+        if constexpr(requires{
+            a.allocate_at_least(n);
+        }){
+            return a.allocate_at_least(n);
+        }else{
+            return {a.allocate(n), n};
+        }
     }
 
     // 释放内存
     static constexpr void deallocate(Alloc& a, pointer p, size_type n)
     {
+        a.deallocate(p, n);
         /// 在此处添加你的实现。
     }
 
@@ -277,6 +310,18 @@ public:
     template <typename T, typename... Args>
     static constexpr void construct(Alloc& a, T* p, Args&&... args)
     {
+
+        if constexpr(requires{
+            a.construct(p, args...);
+        }){
+            a.construct(p, args...);
+        }else if constexpr(requires{
+            std::is_constructible_v<T, Args...>;
+        }){
+            std::construct_at(p, std::forward<Args>(args)...);
+        }else{
+	      std::__throw_bad_array_new_length();
+        }
         /// 在此处添加你的实现。
     }
 
@@ -284,12 +329,28 @@ public:
     template <typename T>
     static constexpr void destroy(Alloc& a, T* p)
     {
+        if constexpr(requires{
+            a.destroy(p);
+        }){
+            a.destroy(p);
+        }else if constexpr(requires{
+            std::is_destructible_v<T>;
+        }){
+            std::destroy_at(p);
+        }
         /// 在此处添加你的实现。
     }
 
     // 获取最大可分配的元素数量
     static constexpr size_type max_size(const Alloc& a) noexcept
     {
+        if constexpr(requires{
+            a.max_size();
+        }){
+            return a.max_size();
+        }
+        // static_assert(std::is_same_v<size_type, long int>, "is same as");
+        return std::numeric_limits<size_type>::max()/sizeof(value_type);
         /// 在此处添加你的实现。
     }
 
@@ -298,9 +359,13 @@ public:
     static constexpr Alloc select_on_container_copy_construction(const Alloc& a)
     {
         /// 在此处添加你的实现。
-        return {};
+        if constexpr(requires{
+            a.select_on_container_copy_construction();
+        }){
+            return a.select_on_container_copy_construction();
+        }
+        return a;
     }
-
 
 };
 }
